f53e5861f4259858abe6fb88684f2e646aea62f4
Jason Kuo <j20001970@gmail.com>
2020-10-07 15:46:52 +0800

    time complexity comment for selection and insert sort

--- insert.py
+++ insert.py
@@ -4,17 +4,17 @@ arr = list(range(10))
 random.shuffle(arr)
 print(arr)
 
-for selected in range(1, len(arr)):
-    end = selected
-    for comp in range(selected-1, -1, -1):
+for selected in range(1, len(arr)): # n-1
+    dest = selected # n-1
+    for comp in range(selected-1, -1, -1): # (1, 2, ..., n-1) -> ((1+(n-1))*n-1)/2 = (n^2-n)/2
         if(arr[comp] > arr[selected]):
-            end = comp
+            dest = comp # (n^2-n)/2
         else:
             break
-    tmp = arr[selected]
-    for i in range(selected, end, -1):
-        aux=arr[i-1]
-        arr[i-1]=arr[i]
-        arr[i]=aux
-    arr[end] = tmp
+    tmp = arr[selected] # n-1
+    for i in range(selected, dest, -1): # (n-1)*?
+        arr[i-1], arr[i] = arr[i], arr[i-1] # (n-1)*?
+    arr[dest] = tmp # n-1
     print(arr)
+
+    # 6*(n-1) + 2*((n^2-n)/2) = n^2-n+6n-6 = n^2+5n-6, O(n^2)
--- selection.py
+++ selection.py
@@ -4,12 +4,12 @@ arr = list(range(10))
 random.shuffle(arr)
 print(arr)
 
-for dest in range(len(arr)-1):
-    selected = dest
-    for i in range(dest+1, len(arr)):
+for dest in range(len(arr)-1): # n-1
+    selected = dest # n-1
+    for i in range(dest+1, len(arr)): # (n-1, n-2, ..., n-(n-1)) -> ((1+n-1)*(n-1))/2 = (n^2-n)/2
         if arr[selected] > arr[i]:
-            selected = i
-    aux = arr[selected]
-    arr[selected]=arr[dest]
-    arr[dest] = aux
+            selected = i # (n^2-n)/2
+    arr[selected], arr[dest] = arr[dest], arr[selected] # n-1
     print(arr)
+
+    # 3*(n-1) + 2*((n^2-n)/2) = n^2-n+3n-3 = n^2+2n-3, O(n^2)
