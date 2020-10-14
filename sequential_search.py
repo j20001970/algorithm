1c77e90f0f0db29e54aff050335cf3bc0e178035
Jason Kuo <j20001970@gmail.com>
2020-10-14 19:23:41 +0800

    Sequential Search

---
+++ sequential_search.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+
+data = [20, 31, 50, 17, 16, 36, 19, 8]
+
+def sequential_search(data, key):
+    for i in data:
+        if i == key:
+            print("found")
+            return
+    else:
+        print("not found")
+
+user_input = int(input("Enter number"))
+sequential_search(data, user_input)
