7f69ae092a69f6465c06fb5f3c9ace8003b50156
Jason Kuo <j20001970@gmail.com>
2020-10-14 20:16:03 +0800

    add sorting and user input

--- bsearch.py
+++ bsearch.py
@@ -1,6 +1,18 @@
 #1/usr/bin/python
 
-data = [5, 8, 9, 15, 30]
+def insert_sort(arr):
+    for selected in range(1, len(arr)): # n-1
+        dest = selected # n-1
+        for comp in range(selected-1, -1, -1): # (1, 2, ..., n-1) -> ((1+(n-1))*n-1)/2 = (n^2-n)/2
+            if(arr[comp] > arr[selected]):
+                dest = comp # (n^2-n)/2
+            else:
+                break
+        tmp = arr[selected] # n-1
+        for i in range(selected, dest, -1): # (n-1)*?
+            arr[i-1], arr[i] = arr[i], arr[i-1] # (n-1)*?
+        arr[dest] = tmp # n-1
+    return arr
 
 def bsearch(data, key, l=None, r=None):
     l = 0 if l == None else l
@@ -15,7 +27,18 @@ def bsearch(data, key, l=None, r=None):
     else:
         return m
 
-result = bsearch(data, 9)
+data = input("Enter 5 number separated with space: ")
+data = data.split(" ")[:5]
+for i, d in enumerate(data):
+    data[i] = int(d)
+
+print("排序前", data)
+
+data = insert_sort(data)
+print("排序後", data)
+
+user_input = int(input("Enter a number to search: "))
+result = bsearch(data, user_input)
 
 if result != None:
     print("found at index %d" % result)
